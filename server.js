c2f23ec6f81e398530f2c331084cf8f23915004c
root <root@AZWEWSDLWWHR3.localdomain>
2025-11-27 19:24:59 +0600

    Page tittle

--- server.js
+++ server.js
@@ -9,12 +9,19 @@ app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>Hello! Your Node.js app is running on Heroku 🚀</h1>
-    <img src="my_photo.jpg" alt="My Photo" style="max-width: 100%; height: auto;" />
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Module - 02 | Batch 08</title>
+    </head>
+    <body>
+        <h1>Hello! Your Node app is running on Heroku</h1>
+        <img src="my_photo.jpg" alt="My Photo" style="max-width: 100%; height: auto;" />
+    </body>
+    </html>
   `);
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
