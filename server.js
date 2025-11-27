1216da3a883728280a93187b514e98ca1227dbdf
root <root@AZWEWSDLWWHR3.localdomain>
2025-11-27 16:40:39 +0600

    Photo_Added

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 // server.js
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files (including your photo)
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
-  res.send('Hello World! Your Node.js app is running on Heroku 🚀');
+  res.send(`
+    <h1>Hello! Your Node.js app is running on Heroku 🚀</h1>
+    <img src="my_photo.jpg" alt="My Photo" style="max-width: 100%; height: auto;" />
+  `);
 });
 
 app.listen(PORT, () => {
